72a131078f50ac613b742404134408420f6e7239
vd2905 <darshanvarsani01@gmail.com>
2023-08-03 11:22:30 +0530

    Final Commit

--- app/src/main/java/com/example/tictac_toe/Game_Activity.java
+++ app/src/main/java/com/example/tictac_toe/Game_Activity.java
@@ -6,80 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Game_Activity extends AppCompatActivity implements View.OnClickListener
 {
-    Button btn[] = new Button[9] ;
+    Button t[] = new Button[9] ;
     int cnt = 0 ;
-    ArrayList arraylist = new ArrayList() ;
-    String b0,b1,b2,b3,b4,b5,b6,b7,b8;
-    TextView text1 ;
-    void win()
-    {
-        if(b0.equals(b1) && b1.equals(b2) && !b1.equals(""))//012
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b3.equals(b4) && b4.equals(b5) && !b4.equals(""))//345
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b6.equals(b7) && b7.equals(b8) && !b7.equals(""))//678
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b0.equals(b3) && b3.equals(b6) && !b3.equals(""))//036
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b1.equals(b4) && b4.equals(b7) && !b4.equals(""))//147
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b2.equals(b5) && b5.equals(b8) && !b5.equals(""))//258
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b0.equals(b4) && b4.equals(b8) && !b4.equals(""))//048
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-        else if(b2.equals(b4) && b4.equals(b6) && !b4.equals(""))//246
-        {
-            disable();
-            Toast.makeText(this, "X WIN", Toast.LENGTH_SHORT).show();
-        }
-    }
-    void disable()
-    {
-        for (int i = 0; i < btn.length; i++)
-        {
-            btn[i].setEnabled(false);
-        }
-    }
+    ArrayList list = new ArrayList() ;
+    TextView txt1,txt2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        text1 = findViewById(R.id.text1);
-        for (int i=0;i<btn.length;i++)
+        txt1 = findViewById(R.id.result);
+        txt2 = findViewById(R.id.restart);
+
+        for (int i=0;i<t.length;i++)
         {
             int id=getResources().getIdentifier("btn"+i,"id",getPackageName());
-            btn[i]=findViewById(id);
-            btn[i].setOnClickListener(this);
+            t[i]=findViewById(id);
+            t[i].setOnClickListener(this);
         }
     }
 
@@ -87,13 +37,14 @@ public class Game_Activity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view)
     {
 
-        for (int i=0;i<btn.length;i++)
+        for (int i=0;i<t.length;i++)
         {
-            if(view.getId()==btn[i].getId())
+            if(view.getId()==t[i].getId())
             {
-                btn[i].setText("X");
-                arraylist.add(i);
-                btn[i].setEnabled(false);
+                t[i].setText("X");
+                list.add(i);
+                t[i].setEnabled(false);
+                win();
                 cnt++;
 
                 if(cnt<=4)
@@ -103,44 +54,228 @@ public class Game_Activity extends AppCompatActivity implements View.OnClickList
                         int min=0;
                         int max=8;
                         int r = new Random().nextInt(max - min) + min;
-                        if(!arraylist.contains(r))
+                        if(!list.contains(r))
                         {
-                            btn[r].setText("0");
-                            arraylist.add(r);
-                            btn[r].setEnabled(false);
-                            break;
-                        }
-                        b1 = btn[1].getText().toString();
-                        b2 = btn[2].getText().toString();
-                        b3 = btn[3].getText().toString();
-                        b4 = btn[4].getText().toString();
-                        b5 = btn[5].getText().toString();
-                        b6 = btn[6].getText().toString();
-                        b7 = btn[7].getText().toString();
-                        b8 = btn[8].getText().toString();
-                        b0 = btn[0].getText().toString();
-
-                        if(cnt>1)
-                        {
+                            t[r].setText("0");
+                            list.add(r);
+                            t[r].setEnabled(false);
                             win();
+                            break;
                         }
                     }
                 }
             }
 
         }
-        text1.setOnClickListener(new View.OnClickListener() {
+        txt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                for (int i = 0; i < btn.length; i++)
+                for (int i = 0; i < t.length; i++)
                 {
-                    btn[i].setText("");
-                    btn[i].setEnabled(true);
-                    arraylist.clear();
+                    t[i].setText("");
+                    list.clear();
                     cnt=0;
+                    t[i].setEnabled(true);
+                    txt1.setText("");
                 }
             }
         });
+    }
+
+    private void win() {
+        if(t[0].getText().toString() == "X" && t[1].getText().toString() == "X" && t[2].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[0].getText().toString() == "X" && t[3].getText().toString() == "X" && t[6].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[0].getText().toString() == "X" && t[4].getText().toString() == "X" && t[8].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[1].getText().toString() == "X" && t[4].getText().toString() == "X" && t[7].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[2].getText().toString() == "X" && t[4].getText().toString() == "X" && t[6].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[2].getText().toString() == "X" && t[5].getText().toString() == "X" && t[8].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[3].getText().toString() == "X" && t[4].getText().toString() == "X" && t[5].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[6].getText().toString() == "X" && t[7].getText().toString() == "X" && t[8].getText().toString() == "X"){
+            txt1.setText("Congratulation ! You Win");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+
+        if(t[0].getText().toString() == "O" && t[1].getText().toString() == "O" && t[2].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[0].getText().toString() == "O" && t[3].getText().toString() == "O" && t[6].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[0].getText().toString() == "O" && t[4].getText().toString() == "O" && t[8].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[1].getText().toString() == "O" && t[4].getText().toString() == "O" && t[7].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[2].getText().toString() == "O" && t[4].getText().toString() == "O" && t[6].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[2].getText().toString() == "O" && t[5].getText().toString() == "O" && t[8].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[3].getText().toString() == "O" && t[4].getText().toString() == "O" && t[5].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
+        if(t[6].getText().toString() == "O" && t[7].getText().toString() == "O" && t[8].getText().toString() == "O"){
+            txt1.setText("Oops !! You lose...");
+            t[0].setEnabled(false);
+            t[1].setEnabled(false);
+            t[2].setEnabled(false);
+            t[3].setEnabled(false);
+            t[4].setEnabled(false);
+            t[5].setEnabled(false);
+            t[6].setEnabled(false);
+            t[7].setEnabled(false);
+            t[8].setEnabled(false);
+        }
     }
 }
